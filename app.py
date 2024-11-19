d00a7715ec2039128ca09c90f4d9362be5caef84
Moritz Homann <moritzhomann@Mac.fritz.box>
2024-11-19 09:10:35 +0100

    liimit

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ def get_time():
 @app.route('/api/abfahrten')
 def abfahrten():
     # Abruf der Abfahrtsdaten von Wieckestraße
-    wieckestraße_url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=wiekestra%C3%9Fe&vz=0&ort=Dresden&lim=12"
+    wieckestraße_url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=wiekestra%C3%9Fe&vz=0&ort=Dresden&lim=20"
     wieckestraße_response = requests.get(wieckestraße_url)
     wieckestraße_data = wieckestraße_response.json()
 
     # Abruf der Abfahrtsdaten vom Wasaplatz
-    wasaPlatz_url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=wasaplatz&vz=0&ort=Dresden&lim=30"
+    wasaPlatz_url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=wasaplatz&vz=0&ort=Dresden&lim=50"
     wasaPlatz_response = requests.get(wasaPlatz_url)
     wasaPlatz_data = wasaPlatz_response.json()
 
